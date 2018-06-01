018ec2bd4eaaa254b9cf607da7eec998f6ca85cd
Jack Greenberg <theProgrammerJack@gmail.com>
2018-06-01 18:54:48 -0400

    Added basic project structure and main robot class.

---
+++ src/main/java/org/team639/robot/Robot.java
@@ -0,0 +1,10 @@
+package org.team639.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * Created by Jack Greenberg <theProgrammerJack@gmail.com> on 6/1/2018.
+ * Part of 2018-offseason-experiments.
+ */
+public class Robot extends TimedRobot {
+}
